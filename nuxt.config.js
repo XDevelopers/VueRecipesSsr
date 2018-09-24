a1e9b55ccc6c184c0e5800d873f793418d681a99
jan pungarsek <jan.pungarsek@gmail.com>
2018-09-24 18:51:27 +0200

    configured dynamic routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,5 +69,49 @@ module.exports = {
 		extend(config, ctx) {
 
 		}
+	},
+	generate: {
+		routes: function () {
+			return new Promise((resolve, reject) => {
+
+				resolve({
+					data: [
+						{
+							id: '1',
+							title: 'Chicken Tikka Masala',
+							description: 'Super tasty chicken',
+							image:
+								'https://img.delicious.com.au/tipUVl-4/h506-w759-cfill/del/2015/10/chicken-tikka-masala-skewers-with-coriander-dressing-13113-1.jpg',
+						},
+						{
+							id: '2',
+							title: 'Taco',
+							description: 'Super tasty taco',
+							image:
+								'http://d2gtpjxvvd720b.cloudfront.net/system/recipe/image/992/default_hg-i-hungry-spaghetti-tacos.jpg',
+						},
+						{
+							id: '3',
+							title: 'Chicken Nuggets',
+							description: 'Super tasty chicken nuggets',
+							image: 'https://5.imimg.com/data5/BE/JR/GLADMIN-40426501/chicken-nuggets-500x500.png',
+						},
+						{
+							id: '4',
+							title: 'Bacon Wrapped Chicken',
+							description: 'Super tasty chicken with bacon',
+							image:
+								'https://www.ditchthecarbs.com/wp-content/uploads/2014/02/rsz_bacon_covered_chicken_nuggets-2.jpg',
+						},
+					],
+				});
+			}).then((res) => {
+				return res.data.map((recipe) => {
+					console.log(recipe);
+
+					return '/recipes/' + recipe.id
+				})
+			})
+		}
 	}
 }
