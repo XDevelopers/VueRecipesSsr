29ab38d030de8aac509e5cb2053d209128eadf27
jan pungarsek <jan.pungarsek@gmail.com>
2018-09-24 06:58:47 +0200

    baseurl

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,7 @@ const pkg = require('./package')
 module.exports = {
 	mode: 'universal',
 	router: {
+		baseUrl: '/VueRecipesSsr/'
 	},
 	env: {
 		baseUrl: process.env.BASE_URL || 'http://localhost:3000'
